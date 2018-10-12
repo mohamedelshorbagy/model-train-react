b29ad9875a7902c042bdd572eb3756329913e884
shorbagy <mohamedelshorbagy96@gmail.com>
2018-10-12 16:53:48 +0200

    add first production build

---
+++ build/precache-manifest.02337ba496abda7663bd3039b5c7c11a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02f27a5ea453e51ae133",
+    "url": "/static/js/main.02f27a5e.chunk.js"
+  },
+  {
+    "revision": "8224bab13bb2dc9ed209",
+    "url": "/static/js/1.8224bab1.chunk.js"
+  },
+  {
+    "revision": "02f27a5ea453e51ae133",
+    "url": "/static/css/main.51c8e564.chunk.css"
+  },
+  {
+    "revision": "a131ea57dcf6d192aef0b9baa94271ea",
+    "url": "/index.html"
+  }
+];
